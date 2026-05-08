2731a85c22e81f12b70236cfe7a18778f7cad4c3
denis4murimi <denis4murimi@users.noreply.github.com>
2026-05-08 08:51:07 +0000

    deploy: 8c6ff0c113acb44205b4857014f1c3c52fbd11b4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2074654016" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "561368413" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
